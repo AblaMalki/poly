efaef5630f18f6d511f261028aede534b01e917b
Abla Malki <109620464+AblaMalki@users.noreply.github.com>
2023-03-13 10:06:01 +0100

    fixedddd

--- back/index.js
+++ back/index.js
@@ -1,33 +1,30 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-var { config } = require("dotenv");
-const bin = require("./routes/Bin");
-const citizen_Request = require("./routes/Citizen_Request");
-const user = require("./routes/User")
-const secteur = require("./routes/Secteur");
-const type_pdc = require("./routes/TypePdc")
-var cors = require("cors");
-var cookieParser = require("cookie-parser");
+var { config } = require('dotenv');
+const bin = require('./routes/Bin');
+const citizen_Request = require('./routes/Citizen_Request');
+const user = require('./routes/User');
+const secteur = require('./routes/Secteur');
+const type_pdc = require('./routes/TypePdc');
+var cors = require('cors');
+var cookieParser = require('cookie-parser');
 
-
-const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.NODE_ENV !== 'production';
 if (dev) {
   config();
 }
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 app.use(cors({ credentials: true, origin: true }));
 
-app.use("/bins",bin)
-app.use("/requests",citizen_Request)
-app.use("/users", user)
-app.use("/secteurs", secteur)
-app.use("/types", type_pdc)
-
-
+app.use('/bins', bin);
+app.use('/requests', citizen_Request);
+app.use('/users', user);
+app.use('/secteurs', secteur);
+app.use('/types', type_pdc);
 
 app.listen(process.env.PORT, (err) => {
-    if (err) throw err;
-    console.log("> Ready on http://localhost:" + process.env.PORT);
+  if (err) throw err;
+  console.log('> Ready on http://localhost:' + process.env.PORT || 3001);
 });
--- back/models/Bin.js
+++ back/models/Bin.js
@@ -1,62 +1,64 @@
-const { DataTypes } = require("sequelize");
+const { DataTypes } = require('sequelize');
 
-const sequelize = require('../db')
+const sequelize = require('../db');
 
-const Secteur = require('./Secteur')
-const Type_pdc = require('./TypePdc')
- 
-const Bin = sequelize.define("bins", {
-   srb: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: true
-   },
-   nom_pdc: {
-      type: DataTypes.STRING,
-      allowNull: true
-   },
-   zone: {
-      type: DataTypes.STRING,
-      allowNull: true
-   },
-   latitude: {
-      type: DataTypes.STRING,
-    allowNull: true
-   },
-   longitude: {
-      type: DataTypes.STRING,
-      allowNull: true
-   },
-   date_creation: {
-      type: DataTypes.DATE,
-      allowNull: true
-   },
-   date_deploiement: {
-      type: DataTypes.DATE,
-      allowNull: true
-   },
-   etat: {
-      type: DataTypes.ENUM("Actif","Pending"),
-      defaultValue: "Pending",
-      allowNull: true
-   },
-   deleted: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-   }
+const Secteur = require('./Secteur');
+const Type_pdc = require('./TypePdc');
 
+const Bin = sequelize.define('bins', {
+  srb: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: true,
+  },
+  nom_pdc: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  zone: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  latitude: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  longitude: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  date_creation: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+  date_deploiement: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+  etat: {
+    type: DataTypes.ENUM('Actif', 'Pending'),
+    defaultValue: 'Pending',
+    allowNull: true,
+  },
+  deleted: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
 });
 
-Type_pdc.hasMany(Bin)
-Bin.belongsTo(Type_pdc)
-Secteur.hasMany(Bin)
-Bin.belongsTo(Secteur)
+Type_pdc.hasMany(Bin);
+Bin.belongsTo(Type_pdc);
+Secteur.hasMany(Bin);
+Bin.belongsTo(Secteur);
 
-sequelize.sync().then(() => {
+sequelize
+  .sync()
+  .then(() => {
     console.log('Bins table created successfully!');
- }).catch((error) => {
+  })
+  .catch((error) => {
     console.error('Unable to create table : ', error);
- });
+  });
 
- module.exports = Bin
+module.exports = Bin;
